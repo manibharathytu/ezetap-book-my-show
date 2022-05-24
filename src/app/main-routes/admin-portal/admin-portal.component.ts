ef53c170ec8109ba2b4ac2fbf7f10c09de7c851b
manibharathytu5194@gmail.com <manibharathytu5194.com>
2022-05-24 08:11:18 +0530

    changes

--- src/app/main-routes/admin-portal/admin-portal.component.ts
+++ src/app/main-routes/admin-portal/admin-portal.component.ts
@@ -9,15 +9,30 @@ import { AppService } from '../../appservice.service';
   styleUrls: ['./admin-portal.component.scss']
 })
 export class AdminPortalComponent implements OnInit {
+  isShowingNav:boolean;
+  topBar: boolean;
+  arrowSide: string;
 
   constructor(private appService:AppService) { }
 
   ngOnInit() {
     this.appService.changeCurrentPage('adminPage');
+    this.isShowingNav = false;
 
+    this.topBar = false;
+    console.log('ngOnInit change topbar')
+    this.appService.changeTopBar(this.topBar.toString())
+    this.arrowSide = this.topBar?'arrow_upward':'arrow_downward'
 
 
   }
+  toggleTopBar() {
+    console.log('ngOnInit change topbar')
 
+    this.topBar = !this.topBar;
+    this.appService.changeTopBar(this.topBar.toString())
+    if (this.arrowSide == 'arrow_upward') { this.arrowSide = 'arrow_downward' }
+    else if (this.arrowSide == 'arrow_downward') { this.arrowSide = 'arrow_upward' }
+  }
 
 }
--- src/app/nav-bars/sidenav/sidenav.component.ts
+++ src/app/nav-bars/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { AppService } from '../../appservice.service';
 
@@ -11,32 +11,20 @@ export class SidenavComponent implements OnInit {
   events: string[] = [];
   opened: boolean;
   page: string;
-  isShowingNav: boolean;
-  topBar: boolean;
-  arrowSide: string;
+  @Input() isShowingNav: boolean;
+
   loca: string;
   gpsIcon: string;
   constructor(private appService: AppService) { }
 
   ngOnInit() {
     this.page = 'dashboard';
-    this.isShowingNav = false;
 
-    this.topBar = false;
-    console.log('ngOnInit change topbar')
-    this.appService.changeTopBar(this.topBar.toString())
-    this.arrowSide = this.topBar?'arrow_upward':'arrow_downward'
+
     this.changeLocation('bangalore') // gps should work here
   }
 
-  toggleTopBar() {
-    console.log('ngOnInit change topbar')
 
-    this.topBar = !this.topBar;
-    this.appService.changeTopBar(this.topBar.toString())
-    if (this.arrowSide == 'arrow_upward') { this.arrowSide = 'arrow_downward' }
-    else if (this.arrowSide == 'arrow_downward') { this.arrowSide = 'arrow_upward' }
-  }
 
   changeLocation(location) {
     this.loca = location;
